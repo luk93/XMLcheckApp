6f4935c1c53d5826b2aa3f2f381457ddc52c0d3e
luk93 <ligajlukasz93@gmail.com>
2022-08-31 08:19:25 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace XMLcheckApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxXmlPath = new System.Windows.Forms.TextBox();
+            this.buttonCheckXml = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonGetDynText = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripSL = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonGetDataCSExcel = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(454, 12);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select XML Path";
+            // 
+            // textBoxXmlPath
+            // 
+            this.textBoxXmlPath.Location = new System.Drawing.Point(99, 14);
+            this.textBoxXmlPath.Name = "textBoxXmlPath";
+            this.textBoxXmlPath.Size = new System.Drawing.Size(349, 20);
+            this.textBoxXmlPath.TabIndex = 2;
+            // 
+            // buttonCheckXml
+            // 
+            this.buttonCheckXml.Location = new System.Drawing.Point(174, 40);
+            this.buttonCheckXml.Name = "buttonCheckXml";
+            this.buttonCheckXml.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheckXml.TabIndex = 3;
+            this.buttonCheckXml.Text = "Operation 1";
+            this.buttonCheckXml.UseVisualStyleBackColor = true;
+            this.buttonCheckXml.Click += new System.EventHandler(this.buttonCheckXml_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Check Devices in CS XML";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Get Dynamic Textes (with @)";
+            // 
+            // buttonGetDynText
+            // 
+            this.buttonGetDynText.Location = new System.Drawing.Point(174, 66);
+            this.buttonGetDynText.Name = "buttonGetDynText";
+            this.buttonGetDynText.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetDynText.TabIndex = 5;
+            this.buttonGetDynText.Text = "Operation 2";
+            this.buttonGetDynText.UseVisualStyleBackColor = true;
+            this.buttonGetDynText.Click += new System.EventHandler(this.buttonGetDynText_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSL});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 120);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(547, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripSL
+            // 
+            this.toolStripSL.Name = "toolStripSL";
+            this.toolStripSL.Size = new System.Drawing.Size(89, 17);
+            this.toolStripSL.Text = "toolStripStatusLabel1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Get Data From CS excel file";
+            // 
+            // buttonGetDataCSExcel
+            // 
+            this.buttonGetDataCSExcel.Location = new System.Drawing.Point(454, 40);
+            this.buttonGetDataCSExcel.Name = "buttonGetDataCSExcel";
+            this.buttonGetDataCSExcel.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetDataCSExcel.TabIndex = 8;
+            this.buttonGetDataCSExcel.Text = "Operation";
+            this.buttonGetDataCSExcel.UseVisualStyleBackColor = true;
+            this.buttonGetDataCSExcel.Click += new System.EventHandler(this.buttonGetDataCSExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 142);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonGetDataCSExcel);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonGetDynText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCheckXml);
+            this.Controls.Add(this.textBoxXmlPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSearch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "XML Check App by PLRADLIG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxXmlPath;
+        private System.Windows.Forms.Button buttonCheckXml;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonGetDynText;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripSL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonGetDataCSExcel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace XMLcheckApp
+{
+    public partial class Form1 : Form
+    {
+        struct device
+        {
+            public string name;
+            public string index;
+        }
+        List<device> deviceList = new List<device>();
+
+        XmlDocument xmlDoc = new XmlDocument();
+        string xmlPath = "";
+        string excelPath = "";
+        string excelPath2 = "";
+        object misValue = Missing.Value;
+        Excel.Application excelApp;
+        Excel.Workbook excelWb;
+        Excel.Worksheet excelWs;
+        int row = 1;
+        int col = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonGetDataCSExcel.Enabled = false;
+            toolStripSL.Text = "Select or type zenon exported XML path";
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            xmlPath = OpenFile(1);
+            textBoxXmlPath.Text = xmlPath;
+            toolStripSL.Text = "Selected file: " + xmlPath + " Select operation on XML.";
+        }
+        private string OpenFile(int type) // 1- xml, 2 - xlsx
+        {
+            string filePath = "";
+            if (type == 1)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog
+                {
+                    InitialDirectory = @"c:\Users\localadm\Desktop",
+                    Title = "Select XML file",
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    DefaultExt = "xml",
+                    Filter = "Xml file (*.xml)|*.xml",
+                    RestoreDirectory = true,
+                    ReadOnlyChecked = true,
+                    ShowReadOnly = true,
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                }
+            }
+            if (type == 2)
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog
+                {
+                    InitialDirectory = @"c:\Users\localadm\Desktop",
+                    Title = "Select XLS file",
+                    CheckFileExists = true,
+                    CheckPathExists = true,
+                    DefaultExt = "xlsm",
+                    Filter = "Excel file with macro (*.xlsm)|*.xlsm",
+                    RestoreDirectory = true,
+                    ReadOnlyChecked = true,
+                    ShowReadOnly = true,
+                };
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName;
+                }
+            }
+            return filePath;
+        }
+
+        private void buttonCheckXml_Click(object sender, EventArgs e)
+        {
+            toolStripSL.Text = "Processing...";
+            if (xmlPath.Length > 0)
+            {
+                excelApp = new Excel.Application();
+                excelWb = excelApp.Workbooks.Add(misValue);
+                excelWs = excelApp.Worksheets[1];
+                row = 3;
+                col = 1;
+                xmlDoc.Load(xmlPath);
+                SearchXml(xmlDoc.DocumentElement, excelWs, 1);
+                (excelWs.Cells[1, 1] as Excel.Range).Select();
+                SortExcelByCol(excelWs, 2);
+                (excelWs.Cells[1, 1] as Excel.Range).Value = "ZENON EXPORT:";
+                (excelWs.Cells[2, 1] as Excel.Range).Value = "DEVICE:";
+                (excelWs.Cells[2, 2] as Excel.Range).Value = "INDEX:";
+                excelPath = xmlPath.Substring(0, xmlPath.Length - 4) + "_CS_NO.xlsx";
+
+                excelWb.SaveAs(excelPath);
+                excelWb.Close();
+                excelApp.Quit();
+                buttonCheckXml.Text = "DONE";
+                toolStripSL.Text = "Done. Results saved in Excel File: " + excelPath;
+                buttonGetDataCSExcel.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Select XML File or type path of it");
+            }
+        }
+        private void SearchXml(XmlNode sourceNode, Excel.Worksheet ws, int functionType)
+        {
+            //Description:
+            //type 1 - get CS indexes
+            //type 2 - get Dynamic Textes
+
+            foreach (XmlNode myNode in sourceNode.ChildNodes)
+            {
+                //type 1 - get CS indexes
+                if (functionType == 1)
+                {
+                    if (myNode.Name.Contains("Elements_"))
+                    {
+                        string elementName = "";
+                        string elementCSIndex = "";
+
+                        foreach (XmlNode childNode in myNode.ChildNodes)
+                        {
+                            if (String.Equals(childNode.Name, "Name"))
+                            {
+                                elementName = childNode.InnerText;
+                            }
+                            if (String.Equals(childNode.Name, "SubstituteDestination"))
+                            {
+                                elementCSIndex = childNode.InnerText;
+                            }
+                        }
+                        if (elementCSIndex.Contains("[") && elementCSIndex.Contains("]"))
+                        {
+                            string tempText1, tempText2 = "";
+                            tempText1 = elementCSIndex.Substring(0, elementCSIndex.Length - 1);
+                            tempText2 = tempText1.Substring(1, tempText1.Length - 1);
+                            (ws.Cells[row, col] as Excel.Range).Value = elementName;
+                            (ws.Cells[row, col + 1] as Excel.Range).Value = tempText2;
+                            row++;
+                        }
+                    }
+                }
+
+                //type 2 - get Dynamic Textes
+                if (functionType == 2)
+                {
+                    if (myNode.Name.Contains("Text"))
+                    {
+                        string dynText = myNode.InnerText;
+                        if (dynText.Contains("@"))
+                        {
+                            (ws.Cells[row, col] as Excel.Range).Value = dynText;
+                            (ws.Cells[row, col + 1] as Excel.Range).Value = dynText.Substring(1, dynText.Length - 1);
+                            row++;
+                        }
+
+                    }
+                }
+            }
+            foreach (XmlNode childNode in sourceNode.ChildNodes)
+            {
+                SearchXml(childNode, ws, functionType);
+            }
+
+
+        }
+        private void buttonGetDynText_Click(object sender, EventArgs e)
+        {
+            toolStripSL.Text = "Processing...";
+            if (xmlPath.Length > 0)
+            {
+                excelApp = new Excel.Application();
+                excelWb = excelApp.Workbooks.Add(misValue);
+                excelWs = excelApp.Worksheets[1];
+                (excelWs.Cells[1, 1] as Excel.Range).Value = "DYNAMIC TEXTS:";
+                (excelWs.Cells[1, 2] as Excel.Range).Value = "ENG TEXTS:";
+                row = 2;
+                col = 1;
+                xmlDoc.Load(xmlPath);
+                SearchXml(xmlDoc.DocumentElement, excelWs, 2);
+                excelPath = xmlPath.Substring(0, xmlPath.Length - 4) + "_TEXT.xlsx";
+                excelWb.SaveAs(excelPath);
+                excelWb.Close();
+                excelApp.Quit();
+                buttonGetDynText.Text = "DONE";
+                toolStripSL.Text = "Done. Results saved in Excel File: " + excelPath;
+            }
+            else
+            {
+                MessageBox.Show("Select XML File or type path of it");
+            }
+        }
+        private void SortExcelByCol(Excel.Worksheet ws, int col)
+        {
+            ws.UsedRange.Select();
+            ws.Sort.SortFields.Clear();
+            ws.Sort.SortFields.Add(ws.UsedRange.Columns[col], Excel.XlSortOn.xlSortOnValues, Excel.XlSortOrder.xlAscending, Type.Missing, Excel.XlSortDataOption.xlSortNormal);
+            var sort = ws.Sort;
+            sort.SetRange(ws.UsedRange);
+            //sort.Header = Excel.XlYesNoGuess.xlYes; - no header
+            sort.MatchCase = false;
+            sort.Orientation = Excel.XlSortOrientation.xlSortColumns;
+            sort.SortMethod = Excel.XlSortMethod.xlPinYin;
+            sort.Apply();
+        }
+        private string doubleToStringName(double devNo)
+        {
+            string devNoString = "";
+            devNoString = devNo.ToString();
+            return devNoString;
+        }
+        private string convertToString(Excel.Range cell)
+        {
+            string result = "";
+            if (cell.Value is double)
+            {
+                result = doubleToStringName(cell.Value);
+            }
+            else result = (string)cell.Value;
+            return result;
+        }
+
+        private void buttonGetDataCSExcel_Click(object sender, EventArgs e)
+        {
+            if (excelPath.Length > 1)
+            {
+                row = 4;
+                col = 6;
+                int index = 0;
+                string cell = "";
+                string cell2 = "";
+
+                excelPath2 = OpenFile(2);
+                toolStripSL.Text = "Processing...";
+                excelApp = new Excel.Application();
+                excelWb = excelApp.Workbooks.Open(excelPath2, false, true);
+                excelWs = excelApp.Worksheets["ResultX"];
+                excelApp.Visible = false;
+                cell = convertToString(excelWs.Cells[row, col] as Excel.Range);
+                do
+                {
+                    if (cell != null)
+                    {
+                        if (cell.Length > 0)
+                        {
+                            deviceList.Add(new device());
+                            var dev = deviceList[index];
+                            dev.name = cell;
+                            cell2 = convertToString(excelWs.Cells[row, col + 4] as Excel.Range);
+                            dev.index = cell2;
+                            deviceList[index] = dev;
+                            index++;
+                        }
+                    }
+                    row++;
+                    cell = convertToString(excelWs.Cells[row, col] as Excel.Range);
+                }
+                while (row <= 800);
+                excelWb.Close();
+                excelWb = excelApp.Workbooks.Open(excelPath);
+                excelWs = excelApp.Worksheets[1];
+                row = 3;
+                col = 4;
+
+                for (int i = 0; i <= deviceList.Count - 1; i++)
+                {
+                    var tempDev = deviceList[i];
+                    (excelWs.Cells[row, col] as Excel.Range).Value = tempDev.name;
+                    (excelWs.Cells[row, col + 1] as Excel.Range).Value = tempDev.index;
+                    row++;
+                }
+                (excelWs.Cells[1, 4] as Excel.Range).Value = "CS EXCEL:";
+                (excelWs.Cells[2, 4] as Excel.Range).Value = "DEVICE";
+                (excelWs.Cells[2, 5] as Excel.Range).Value = "INDEX";
+                excelWb.SaveAs(excelPath);
+                excelWb.Close();
+                excelApp.Quit(); 
+                buttonGetDataCSExcel.Text = "DONE";
+                toolStripSL.Text = "Done. Results saved in Excel File: " + excelPath;
+            }
+            else
+            {
+                MessageBox.Show("Operation 1 must be done to select this Operation!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
